ff7b31fb48be55b2ccfccf26665c3b75ffb9b21f
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-02-06 16:51:29 +0200

    m

--- express-exercise/index.js
+++ express-exercise/index.js
@@ -6,6 +6,12 @@ app.use(express.json());
 
 let PORT = process.env.PORT || 8001;
 
+let users = [
+    {id: 1, name: 'Ty', age: 22},
+    {id: 2, name: 'Joel', age: 35},
+    {id: 3, name: 'Matthew', age: 23},
+]
+
 // Middleware function
 function middleware(req, res, next) {
     console.log('MIDDLEWARE');
@@ -18,19 +24,21 @@ app.get('/', (req, res) => {
     // 1. return all users in array
     res.send(users);
 });
-
-let users = [
-    {id: 1, name: 'Ty', age: 22},
-    {id: 2, name: 'Joel', age: 35},
-    {id: 3, name: 'Matthew', age: 23},
-]
 // End of Middleware
 
-function log(req, res, next) {
-    console.log('I am responding');
-    next();
-};
+//2. Calling via an ID
+app.get('/users/:id', (req, res) => {
+    let user = users.find(friend => friend.id === +req.params.id);
+    res.send(user);
+});
 
+// 2. Add new people into the array
+app.post('/users', (req, res) => {
+    
+});
+
+
+// App.Listen host my server
 app.listen( PORT, () => {
     console.log(`the server is RUNNING ON http://localhost:${PORT}`);
 });
